2d92a10de21fe78892f3b4fe8a52f9e2fe96f93f
Vishal Kumar Jangid <imrvishal28@gmail.com>
2024-11-29 09:52:12 +0530

    enhance currency converter UI with favicon, improved layout, and additional functionality for amount conversion

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const dropList = document.querySelectorAll(".drop-list select");
 
 for (let i = 0; i < dropList.length; i++) {
   for (let currency_code in country_list) {
-    let selected;
-    if (i == 0) {
-      selected = currency_code == "USD" ? "selected" : "";
-    } else if (i == 1) {
-      selected = currency_code == "INR" ? "selected" : "";
+    let selected = "";
+    if (i === 0 && currency_code === "USD") {
+      selected = "selected";
+    } else if (i === 1 && currency_code === "INR") {
+      selected = "selected";
     }
-    let optionTag = `<option value="${currency_code}">${currency_code}</option>`;
+    let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
     dropList[i].insertAdjacentHTML("beforeend", optionTag);
   }
 }
